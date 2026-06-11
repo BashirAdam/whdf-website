8d936a2374f6cbee9b8cc8d7c263b3f05053c95a
BashirAdam <BashirAdam@users.noreply.github.com>
2026-06-11 18:03:27 +0300

    now we fixd also the text algin without anything

--- frontend/src/components/Programs.jsx
+++ frontend/src/components/Programs.jsx
@@ -420,13 +420,9 @@ const Programs = () => {
     <h3 className="text-xl font-bold text-gray-800">  الدكتور ابوبكر برقو  </h3>
   </div>
   <p className="text-base text-[#d78525] mb-3">Dr. Abubakr Bargou</p>
-  <p 
-  className="text-gray-600 leading-relaxed text-justify rtl:text-right" 
-  dir="rtl"
-  lang="ar"
->
+  <p className="text-gray-600 leading-relaxed"> 
   دكتوراة في الإعلام من الجامعة الاميركية بالقاهرة، المستشار الإعلامي الأسبق للرئيس الراحل إدريس ديبي والمستشار المكلف بالبعثات الدبلوماسية السابق للرئيس محمد إدريس ديبي. رئيس أتحاد التلفزة الأفريقي ومؤسس اذاعة النصر (كأول مؤسس إذاعة وتلفزيون من أبناء الكيان)، وكاتب ومحلل سياسي متخصص في الشؤون الأفريقية
-</p>
+  </p>
 </div>
             </div>
 
